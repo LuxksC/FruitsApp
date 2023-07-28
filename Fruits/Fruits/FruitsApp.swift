bf11c82b2edeaab91abc6840c3234e7c479b8869
Lucas de Castro <luscasdev@gmail.com>
2023-07-28 13:59:14 -0300

    Add assets resources

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Lucas de Castro Souza on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
